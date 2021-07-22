42b15b2a0874d7ae3ec03c09a3e7ff9988936fe5
clauCampos <82203430+clauCampos@users.noreply.github.com>
2021-07-22 17:16:54 +0200

    restored app

--- web-client/src/js/components/NavigationBar.jsx
+++ web-client/src/js/components/NavigationBar.jsx
@@ -1,11 +1,23 @@
 import * as React from "react";
 import {Link} from "react-router-dom";
-
+import logo from './assets/econoTravel-logo.png';
 export const NavigationBar = () =>(
-
     <nav className={"navigationBar"}>
-        <Link to={"/Catalog"}>Catalogo</Link>
-        <Link to={"/about"}>Quienes somos</Link>
+        <img className={"logo"} src={logo} alt={'EconoTravel logo'} />
+        <div className={"menu__links"}>
+            <Link to={"/home"}>EconoTravel</Link>
+            <Link to={"/about"}>Quienes somos</Link>
+            <Link to={"/Catalog"}>Experiencias</Link>
+            <Link to={"/reservas"}>Reservas</Link>
+        </div>
+        <div className={"searchBar"}>
+            <label htmlFor={"Encuenta tu experiencia"}></label>
+            <input type={"text"} placeholder={"Encuenta tu experiencia"} />
+            <input type={"submit"} value={"Buscar"} />
+        </div>
+        <div className={"socialBar"}>
+            <Link to={"/home"}>Facebook</Link>
+            <Link to={"/about"}>Instagram</Link>
+        </div>
     </nav>
-
 )
--- web-client/webpack.config.js
+++ web-client/webpack.config.js
@@ -39,6 +39,17 @@ module.exports = {
                 }
             },
             {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            // publicPath: 'build',
+                        }
+                    },
+                ]
+            },
+            {
                 test: /\.scss$/,
                 use: [
                     "style-loader",
