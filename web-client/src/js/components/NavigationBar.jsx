4ff28238eef1ff071d2da1a0acc49aeb96b82f71
clauCampos <82203430+clauCampos@users.noreply.github.com>
2021-07-22 15:38:43 +0200

    last try

--- web-client/src/js/components/NavigationBar.jsx
+++ web-client/src/js/components/NavigationBar.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import {Link} from "react-router-dom";
-import logo from './assets/econoTravel-logo.svg';
+import logo from './assets/econoTravel-logo.png';
 
 export const NavigationBar = () =>(
 
