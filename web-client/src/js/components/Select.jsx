b82c62321622d26813492a71b9eec930f64ba6eb
clauCampos <82203430+clauCampos@users.noreply.github.com>
2021-07-27 20:53:30 +0200

    select Bar styles

--- web-client/src/js/components/Select.jsx
+++ web-client/src/js/components/Select.jsx
@@ -5,23 +5,24 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 export const Select = () => {
     return(
-        <div className="select-bar">
-            <FloatingLabel controlId="floatingSelect" label="Works with selects">
-                <h4>Buscas un tour? te ayudamos</h4>
-                <Form.Select aria-label="Floating label select example">
+        <div className="select__bar">
+            <FloatingLabel controlId="floatingSelect" className="select-bar">
+                <h4 className={"title__search"}>Buscas una experiencia?  Te ayudamos</h4>
+                <div className="select__form">
+                    <Form.Select  className={"select__group"} aria-label="Floating label select example">
                     <option>Experiencias</option>
                     <option value="1">Paseo en bicicleta por el Montseny</option>
                     <option value="2">Descubre la costa Brava en barco de vela</option>
                     <option value="3">Descubre la Barcelona Modernista de noche</option>
-                </Form.Select>
-
-                <Form.Select aria-label="Floating label select example">
-                    <option>Tipos de tours</option>
-                    <option value="1">Barcos</option>
-                    <option value="2">Bicicleta</option>
-                    <option value="3">Caminata</option>
-                </Form.Select>
-                <Button className="btn btn-outline-success btn-green" variant="success" >Buscar! </Button>
+                    </Form.Select>
+                    <Form.Select className={"select__group"} aria-label="Floating label select example">
+                        <option>Tipos de tours</option>
+                        <option value="1">Barcos</option>
+                        <option value="2">Bicicleta</option>
+                        <option value="3">Caminata</option>
+                    </Form.Select>
+                    <Button className="btn btn-outline-success btn-search" variant="success" >Buscar! </Button>
+                </div>
             </FloatingLabel>
 
 
