9c799a892c1251992e54207993c4829d22fe6a1e
pamela salazar <you@example.com>
2021-07-28 15:07:19 +0200

    Yeraldin: Cambio div por section

--- web-client/src/js/components/Reservation.jsx
+++ web-client/src/js/components/Reservation.jsx
@@ -7,11 +7,11 @@ import {ImagenesFooter} from "./ImagenesFooter";
 
 export const Reservation = () => {
   return (
-      <>
+      <section>
           <p> Aqui deberia ir mas contenio </p>
           <ImagenesFooter />
 
-      </>
+      </section>
 
   )
 }
