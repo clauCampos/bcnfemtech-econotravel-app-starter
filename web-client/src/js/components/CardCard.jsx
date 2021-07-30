7917ce50621fd009d1bf6fdeb71611ee5d351108
pamela salazar <you@example.com>
2021-07-30 10:07:26 +0200

    Yeraldin cambios miniCard

--- web-client/src/js/components/CardCard.jsx
+++ web-client/src/js/components/CardCard.jsx
@@ -3,28 +3,25 @@ import {Button, Card} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
-export const CardCard= () => {
+export const CardCard = (props) => {
     return (
-        <section>
-            <Card>
-                <div>
-                    <Card.Img />
-                </div>
-                <div>
-                    <Card.Img />
-                </div>
-                <div>
-                    <Card.Img />
-                </div>
-                <div>
-                    <Card.Img />
-                </div>
+        <div className={"card__details"}>
+            <Card className={"card-container animate__animated animate__zoomIn card__item"}>
+                <Card.Body className={"card__body"}>
 
+                    <Card.Img className={"Sailing"} src={props.image} alt={"Sailing"}/>
+                    <Card.Text className='texto'>holii</Card.Text>
+
+                    <Card.Img className={"icon"} src={props.beach} alt={"Beach"}/>
+                    <Card.Text className={"icon__text"}>try</Card.Text>
+
+                    <Button to={"/Experience"} className={"btn__card"}>Reserva Ya!</Button>
+                    <p>Pasarela para silla de ruedas disponible bajo reserva.</p>
+                </Card.Body>
             </Card>
-
-
-        </section>
+        </div>
 
 
     )
 }
+
--- web-client/src/js/components/MiniCard.jsx
+++ web-client/src/js/components/MiniCard.jsx
@@ -1,13 +1,30 @@
 import * as React from "react";
-import {Button, Card} from "react-bootstrap";
+import {CardCard} from "./CardCard";
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Link} from "react-router-dom";
+import imgSailing from './assets/sailing.png'
+import beach from './assets/playa.png'
 
+const card = [
+    {
+        id: 58,
+        image: imgSailing,
+        beach: beach
 
-export const MiniCard = (props) => {
+    }
+]
+
+export function MiniCard() {
     return (
-        <section className={"cardsSection"}>
-
-        </section>
+        <div className="container">
+            <div className="row">
+                {
+                    card.map(newCard => (
+                        <div className="col-md-5" key={newCard.id}>
+                            <CardCard image={newCard.image} imge2={newCard.beach} />
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
     )
 }
