1aece37fe0093cb3517e28083148ddfc043fbd68
clauCampos <82203430+clauCampos@users.noreply.github.com>
2021-07-31 23:27:05 +0200

    reservas icon list inline

--- web-client/src/js/components/Reservation.jsx
+++ web-client/src/js/components/Reservation.jsx
@@ -16,16 +16,17 @@ export const Reservation = () => {
                         <p className="reservas__text--intro">Disfruta de un hermoso paseo en bicicleta por el increíble Parque Natural del Montseny.
                             Una escapada veraniega perfecta para parejas, familias y amigos que nos permitirá conocer
                             nuevos y sorprendentes lugares.</p>
-                        <p>El recorrido que os proponemos comienza con una larga subida con algunos descansos, combinando
+                        <p className="reservas__text">El recorrido que os proponemos comienza con una larga subida con algunos descansos, combinando
                             asfalto y pistas anchas, culminando en un mirador con magníficas vistas del Vallés y el mar.
                             Desde aquí continuaremos la bajada combinando senderos, pistas y algún tramo de carretera y
                             terreno mixto para visitar la zona de Santa Fe, donde descubriremos la Ermita y el Bosque de las Secuoyas.</p>
-                        <p> Para finalizar, acudiremos al restaurante María Rosa de Palautordera, donde disfrutaremos de una excelente comida casera
+                        <p className="last"> Para finalizar, acudiremos al restaurante María Rosa de Palautordera, donde disfrutaremos de una excelente comida casera
                             con butifarra blanca y negra y munxetes del Montseny.</p>
                         <div className={"reservas__icons"}>
                             <MiniCard />
                         </div>
                     </div>
+
                     <div className={"reservas__form"}>
                         <h4>Haz tu reserva!</h4>
                         <FormikContainer />
